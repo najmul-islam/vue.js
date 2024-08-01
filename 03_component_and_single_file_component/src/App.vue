ec360ca3b420dcbd43a2f66acaaff72ff67be751
najmul-islam <najmulislam519@gmail.com>
2024-08-01 22:03:56 +0600

    component - component in vue - local and global component - use html, css and js in compoent - import component in another componet - use variable in component

---
+++ 03_component_and_single_file_component/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+const name = "Sonam";
+</script>
+
+<template>
+  <div>
+    <h1>Local Registration by {{ name }}</h1>
+    <HelloWorld />
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  text-decoration: underline;
+}
+</style>
---
+++ 03_component_and_single_file_component/src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<script setup>
+import ByeWorld from "./ByeWorld.vue";
+const message = "Hello world";
+</script>
+
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+    <ByeWorld />
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  text-decoration: line-through;
+}
+</style>
