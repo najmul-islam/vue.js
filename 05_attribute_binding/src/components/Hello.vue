25636f1fe4663ef0f9b00a2ab4d176883382f4fa
najmul-islam <najmulislam519@gmail.com>
2024-08-02 12:18:27 +0600

    attribute in vue - static attr - dynamic attr with v-bind or ":" - multple attr with v-bind - dynamic attr name - boolean attr

---
+++ 05_attribute_binding/src/components/Hello.vue
@@ -0,0 +1,34 @@
+<script setup>
+const message = "Hello world";
+const mydataid = "jack";
+const url = "https://google.com";
+const objOfAttr = {
+  id: "Jack",
+  href: "https://google.com",
+};
+
+const attributeName = "href";
+const isDisabled = false;
+</script>
+
+<template>
+  <!-- without v-bind -->
+  <h1>{{ mydataid }}</h1>
+  <h1 id="mydataid">{{ message }}</h1>
+  <h1 id="{{ mydataid }}">id="{{ mydataid }}"</h1>
+  <!-- using v-bind -->
+  <h1 v-bind:id="mydataid">v-bind</h1>
+  <h1 :id="mydataid">v-bind sorthand</h1>
+  <a :href="url">google</a> <br />
+  <!-- obj of attrubute -->
+  <a :id="mydataid" :href="url">multiple v-bind</a><br />
+  <a v-bind="objOfAttr">v-bind with obj</a><br />
+
+  <!-- dynamic argument -->
+  <a v-bind:[attributeName]="url">Dynamic atrribute name</a><br />
+  <a :[attributeName]="url">Dynamic attributeName</a><br />
+
+  <!-- boolean attributes -->
+  <button disabled>Boolean attributes</button>
+  <button :disabled="isDisabled">Boolean attributes</button>
+</template>
