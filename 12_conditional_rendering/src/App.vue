d3960adc3c36e272d94dc2513384efabd7e673a2
najmul-islam <najmulislam519@gmail.com>
2024-09-12 11:13:02 +0600

    conditional rendering in vue

---
+++ 12_conditional_rendering/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import VifDirective from "./components/VifDirective.vue";
+</script>
+
+<template>
+  <h1>Conditional Rendering in Vue Js</h1>
+  <hr />
+  <VifDirective />
+</template>
+
+<style scoped></style>
---
+++ 12_conditional_rendering/src/components/VifDirective.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from "vue";
+
+const isActive = ref(true);
+const num = ref(6);
+const type = ref("A");
+</script>
+
+<template>
+  <!-- <h1 v-if="true">True v-if</h1> -->
+  <!-- <h1 v-if="isActive">isActive is true v-if</h1> -->
+  <!-- <h1 v-if="num === 5">Num is 5 v-if</h1> -->
+  <!-- <h1 v-if="num > 5">Num is getther 5 v-if</h1> -->
+
+  <h1 v-if="isActive">isActive is true v-if</h1>
+  <h1 v-else>This is v-else</h1>
+</template>
