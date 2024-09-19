ae9beb9cff4256804fa81c73108d8c8690662090
najmul-islam <najmulislam519@gmail.com>
2024-09-19 09:55:32 +0600

    event handlening in vue js

---
+++ 14_event_handling/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import EventHandling from "./components/EventHandling.vue";
+</script>
+
+<template>
+  <h1>Event Handling in Vue Js</h1>
+  <hr />
+  <EventHandling />
+</template>
+
+<style scoped></style>
---
+++ 14_event_handling/src/components/EventHandling.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from "vue";
+const count = ref(0);
+
+function greeting() {
+  console.log("Hello world");
+}
+
+function say(e) {
+  console.log(e);
+}
+</script>
+
+<template>
+  <div>
+    <!-- inline handler -->
+    <h1>{{ count }}</h1>
+    <!-- <button v-on:click="count++">Increase</button> -->
+    <!-- <button @click="count++">Increase</button> -->
+    <!-- <input v-on:focus="count++" type="text" /> -->
+
+    <!-- <button @click="greeting()">Hello world</button> -->
+
+    <!-- method handler -->
+    <button @click="say">Click me</button>
+  </div>
+</template>
+
+<style></style>
