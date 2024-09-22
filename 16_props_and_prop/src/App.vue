ceff6a39809d9f4b101cf0685ae298b146d2ecd9
najmul-islam <najmulislam519@gmail.com>
2024-09-22 10:36:50 +0600

    prop and prop in vue

---
+++ 16_props_and_prop/src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from "vue";
+import PropsAndProp from "./components/PropsAndProp.vue";
+
+const firstName = ref("Sonam");
+const lastName = ref("Kumari");
+</script>
+
+<template>
+  <h1>Props and Prop Validation in Vue Js</h1>
+  <hr />
+  <!-- static props -->
+  <!-- <PropsAndProp fullName="Sonam Kumari" /> -->
+
+  <!-- dynamic props -->
+  <!-- <PropsAndProp :firstName="firstName" :lastName="lastName" /> -->
+
+  <!-- oneway data flow -->
+  <!-- <PropsAndProp :firstName="firstName" :lastName="lastName" /> -->
+
+  <!-- prop validaiton -->
+  <PropsAndProp fullName="Sonam Kumari" :age="25" />
+</template>
+
+<style scoped></style>
---
+++ 16_props_and_prop/src/components/PropsAndProp.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { computed, ref } from "vue";
+
+// const props = defineProps(["fullName", "age"]);
+// const props = defineProps({
+//   fullName: String,
+//   age: Number,
+// });
+const props = defineProps({
+  fullName: {
+    type: String,
+    required: true,
+    default: "Sonam Sharma",
+  },
+  age: {
+    type: Number,
+  },
+});
+
+// const fname = ref(props.firstName);
+// const fullName = computed(() => props.firstName + " " + props.lastName);
+</script>
+
+<template>
+  <div>
+    <!-- <h1>Name: {{ firstName }} {{ lastName }}</h1> -->
+
+    <h1>Name: {{ fullName }}</h1>
+    <h2>Age: {{ age }}</h2>
+  </div>
+</template>
+
+<style scoped></style>
