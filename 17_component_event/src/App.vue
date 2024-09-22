8da555e34a3b88b8c8cdeabd41b88a0fdb604439
najmul-islam <najmulislam519@gmail.com>
2024-09-22 10:37:03 +0600

    component event in vue

---
+++ 17_component_event/src/App.vue
@@ -0,0 +1,45 @@
+<script setup>
+import ComponentEvent from "./components/ComponentEvent.vue";
+import { ref } from "vue";
+
+let count = ref(0);
+
+const increaseByOne = () => {
+  count.value++;
+  console.log("increase by one");
+};
+
+// declareing emitted event
+const emit = defineEmits({
+  fsubmit: ({ name }) => {
+    if (name) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+});
+
+// event validation
+const submitForm = (name) => {
+  emit("fsubmit", { name });
+  console.log("form submitted", name);
+};
+</script>
+
+<template>
+  <div>
+    <h1>Component Event in Vue Js</h1>
+    <hr />
+    <!-- <h3>Count: {{ count }}</h3> -->
+
+    <!-- <ComponentEvent @increaseCount="count++" /> -->
+    <!-- <ComponentEvent @increase-count="count++" /> -->
+    <!-- <ComponentEvent @increase-count="increaseByOne" /> -->
+    <!-- <ComponentEvent @increase-count="(n) => (count += n)" /> -->
+
+    <ComponentEvent @fsubmit="submitForm" />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ 17_component_event/src/components/ComponentEvent.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from "vue";
+
+const name = ref("");
+</script>
+
+<template>
+  <div>
+    <!-- <button @click="$emit('increaseCount', 11)">Incress</button> -->
+
+    <form @submit.prevent="$emit('fsubmit', name)">
+      <label for="name">Name:</label>
+      <input type="text" id="name" v-model="name" />
+      <button type="submit">Submit</button>
+    </form>
+  </div>
+</template>
+
+<style scoped></style>
