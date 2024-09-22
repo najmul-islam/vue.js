37c74b3a9197d535f97b85071066ff093c08f5e9
najmul-islam <najmulislam519@gmail.com>
2024-09-22 10:37:56 +0600

    provide and inject in vue js

---
+++ 20_provide_and_inject/src/components/Guest.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { inject } from "vue";
+
+const message = inject("message");
+</script>
+
+<template>
+  <div style="border: 1px solid black; padding: 10px">
+    <h1>Guest component</h1>
+    {{ message }}
+  </div>
+</template>
+
+<style scoped></style>
---
+++ 20_provide_and_inject/src/components/User.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Guest from "./Guest.vue";
+</script>
+
+<template>
+  <div style="border: 1px solid black; padding: 10px">
+    <h1>User components</h1>
+
+    <Guest />
+  </div>
+</template>
+
+<style scoped></style>
