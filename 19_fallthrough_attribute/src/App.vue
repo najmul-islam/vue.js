c47eb40cc48b076c706a0f6bd5456e588c5bc0ee
najmul-islam <najmulislam519@gmail.com>
2024-09-22 10:37:41 +0600

    fallthrouht attribue in vue js

---
+++ 19_fallthrough_attribute/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Hello from "./components/Hello.vue";
+
+function handleClick() {
+  console.log("btn click");
+}
+</script>
+
+<template>
+  <!-- attrubute inheritance -->
+  <!-- <Hello class="btn-pink" /> -->
+  <!-- <Hello id="btn-pink" /> -->
+  <!-- <Hello :style="{ backgroundColor: 'red' }" /> -->
+  <Hello @click="handleClick" />
+</template>
+
+<style scoped></style>
---
+++ 19_fallthrough_attribute/src/components/Hello.vue
@@ -0,0 +1,8 @@
+<script setup></script>
+
+<template>
+  <button>Click 1</button>
+  <!-- <button>Click 2</button> -->
+</template>
+
+<style scoped></style>
