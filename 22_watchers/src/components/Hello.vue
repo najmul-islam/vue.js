cffd6084a72867dd1f9fdacbdb6fe3da9484c132
najmul-islam <najmulislam519@gmail.com>
2024-09-29 11:19:49 +0600

    learn about watchers

---
+++ 22_watchers/src/components/Hello.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref, watch } from "vue";
+
+let hartRate = ref(85);
+
+watch(hartRate, (newHartRatevalue, oldHartRateValue) => {
+  console.log("newHartRatevalue: ", newHartRatevalue);
+  console.log("oldHartRateValue: ", oldHartRateValue);
+  if (newHartRatevalue >= 120) {
+    alert("Calm down... take deep breath");
+  }
+});
+</script>
+
+<template>
+  <div>
+    <h1>Hello world</h1>
+    <h2 v-if="hartRate > 0">Hear Beats per Minute:{{ hartRate }}</h2>
+    <h2 v-if="hartRate <= 0">Sorry you are dead...</h2>
+
+    <button @click="hartRate += 5">Increase</button>
+    <button @click="hartRate -= 5">Decrease</button>
+  </div>
+</template>
+
+<style scoped></style>
