51059b7da8c467f00a703c2f6332af8bd5f72120
najmul-islam <najmulislam519@gmail.com>
2024-10-11 08:51:45 +0600

    form validation

---
+++ 04_working_with_data/4.5_validating_forms/src/components/CreatePage.vue
@@ -0,0 +1,82 @@
+<template>
+  <form action="" class="container py-3">
+    <div class="row">
+      <div class="col">
+        <div class="mb-3">
+          <label for="" class="form-label"> Page title </label>
+          <input type="text" class="form-control" v-model="pageTitle" />
+        </div>
+        <div class="mb-3">
+          <label for="" class="form-label"> Content </label>
+          <textarea type="text" class="form-control" rows="5" v-model="content">
+          </textarea>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="mb-3">
+          <label for="" class="form-label"> Link Text </label>
+          <input type="text" class="form-control" v-model="linkText" />
+        </div>
+        <div class="mb-3">
+          <label for="" class="form-label"> Link Url </label>
+          <input type="text" class="form-control" v-model="linkUrl" />
+        </div>
+        <div class="mb-3">
+          <div class="form-check">
+            <label for="" class="form-check-lebel"> Published </label>
+            <input type="checkbox" class="form-check-input" />
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="mb-3">
+      <button
+        class="btn btn-primary"
+        @click.prevent="submitForm"
+        :disabled="isFormInvalid"
+      >
+        Create Page
+      </button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  props: ["pageCreated"],
+  computed: {
+    isFormInvalid() {
+      return (
+        !this.pageTitle || !this.content || !this.linkText || !this.linkUrl
+      );
+    },
+  },
+  data() {
+    return {
+      pageTitle: "",
+      content: "",
+      linkText: "",
+      linkUrl: "",
+    };
+  },
+  methods: {
+    submitForm(e) {
+      if (!this.pageTitle || !this.content || !this.linkText || !this.linkUrl) {
+        alert("Please fill the from");
+        return;
+      }
+
+      this.pageCreated({
+        pageTitle: this.pageTitle,
+        content: this.content,
+        link: {
+          text: this.linkText,
+          url: this.linkUrl,
+        },
+      });
+    },
+  },
+};
+</script>
