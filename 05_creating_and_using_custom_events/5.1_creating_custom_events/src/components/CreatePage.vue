1d3a8d6ee4eebeb404ea27c2ce2984a678569c6e
najmul-islam <najmulislam519@gmail.com>
2024-10-11 08:52:17 +0600

    crate custom event

---
+++ 05_creating_and_using_custom_events/5.1_creating_custom_events/src/components/CreatePage.vue
@@ -0,0 +1,101 @@
+<template>
+  <form action="" class="container py-3">
+    <div class="row">
+      <div class="col">
+        <div class="mb-3">
+          <label for="" class="form-label"> Page title </label>
+          <input type="text" class="form-control" v-model="pageTitle" />
+        </div>
+        <div class="mb-3">
+          <label for="" class="form-label"> Content </label>
+          <textarea type="text" class="form-control" rows="5" v-model="content">
+          </textarea>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="mb-3">
+          <label for="" class="form-label"> Link Text </label>
+          <input type="text" class="form-control" v-model="linkText" />
+        </div>
+        <div class="mb-3">
+          <label for="" class="form-label"> Link Url </label>
+          <input type="text" class="form-control" v-model="linkUrl" />
+        </div>
+        <div class="mb-3">
+          <div class="form-check">
+            <label for="" class="form-check-lebel"> Published </label>
+            <input
+              type="checkbox"
+              class="form-check-input"
+              v-model="published"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="mb-3">
+      <button
+        class="btn btn-primary"
+        @click.prevent="submitForm"
+        :disabled="isFormInvalid"
+      >
+        Create Page
+      </button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  emits: ["pageCreated"],
+  computed: {
+    isFormInvalid() {
+      return (
+        !this.pageTitle || !this.content || !this.linkText || !this.linkUrl
+      );
+    },
+  },
+  data() {
+    return {
+      pageTitle: "",
+      content: "",
+      linkText: "",
+      linkUrl: "",
+      published: true,
+    };
+  },
+  methods: {
+    submitForm(e) {
+      if (!this.pageTitle || !this.content || !this.linkText || !this.linkUrl) {
+        alert("Please fill the from");
+        return;
+      }
+
+      this.$emit("pageCreated", {
+        pageTitle: this.pageTitle,
+        content: this.content,
+        link: {
+          text: this.linkText,
+          url: this.linkUrl,
+        },
+        published: this.published,
+      });
+
+      this.pageTitle = "";
+      this.content = "";
+      this.linkText = "";
+      this.linkUrl = "";
+      this.published = true;
+    },
+  },
+  watch: {
+    pageTitle(newTitle, oldTitle) {
+      if (this.linkText === oldTitle) {
+        this.linkText = newTitle;
+      }
+    },
+  },
+};
+</script>
