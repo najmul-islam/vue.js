3d8943a87587da5ff9918b7998b408825dfcd1a1
najmul-islam <najmulislam519@gmail.com>
2024-10-11 08:50:37 +0600

    start learning from scratch

---
+++ 03_using_the_vue_cli/3.2_starting_a_project_from_scratch/src/components/Navbar.vue
@@ -0,0 +1,50 @@
+<template>
+  <nav
+    :class="[`navbar-${theme}`, `bg-${theme}`, 'navbar', 'navbar-expand-sm']"
+  >
+    <div class="container">
+      <a class="navbar-brand" href="#">Vue js</a>
+
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li :key="page.id" v-for="(page, index) in pages" class="nav-item">
+          <a
+            class="nav-link"
+            aria-current="page"
+            :class="{ active: activePage == index }"
+            :href="page.link.url"
+            :title="`This link goes to the ${page.link.text} page`"
+            @click.prevent="navLinkClick(index)"
+          >
+            {{ page.link.text }}
+          </a>
+        </li>
+      </ul>
+
+      <form class="d-flex">
+        <button class="btn btn-primary" @click.prevent="changeTheme()">
+          Toggle
+        </button>
+      </form>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  props: ["pages", "activePage", "navLinkClick"],
+  data() {
+    return {
+      theme: "light",
+    };
+  },
+  methods: {
+    changeTheme() {
+      if (this.theme === "light") {
+        this.theme = "dark";
+      } else {
+        this.theme = "light";
+      }
+    },
+  },
+};
+</script>
