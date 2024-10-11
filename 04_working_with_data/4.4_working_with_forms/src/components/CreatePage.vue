8b3dadc797c4b5eb5760e6f50225a3ed5455b4fc
najmul-islam <najmulislam519@gmail.com>
2024-10-11 08:51:36 +0600

    form in vue

---
+++ 04_working_with_data/4.4_working_with_forms/src/components/CreatePage.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container mb-3">
+    <form action="">
+      <div class="mb-3">
+        <label for="" class="form-label"> Page title </label>
+        <input type="text" class="form-control" v-model="pageTitle" />
+      </div>
+
+      <div class="mb-3">
+        <label for="" class="form-label"> Content </label>
+        <textarea type="text" class="form-control" rows="5" v-model="content">
+        </textarea>
+      </div>
+
+      <div class="mb-3">
+        <button
+          @click.prevent="pageCreated({ pageTitle, content })"
+          class="btn btn-primary"
+        >
+          Create Page
+        </button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["pageCreated"],
+  data() {
+    return {
+      pageTitle: "",
+      content: "",
+    };
+  },
+};
+</script>
