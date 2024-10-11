1c57bf7fbd713949b2cab25588243e4346fb600f
najmul-islam <najmulislam519@gmail.com>
2024-10-11 08:51:20 +0600

    unset props

---
+++ 04_working_with_data/4.2_working_with_unset_props/src/components/Navbar.vue
@@ -0,0 +1,50 @@
+<template>
+  <nav
+    :class="[`navbar-${theme}`, `bg-${theme}`, 'navbar', 'navbar-expand-sm']"
+  >
+    <div class="container">
+      <a class="navbar-brand" href="#">Vue js</a>
+
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li :key="page.id" v-for="(page, index) in pages" class="nav-item">
+          <navbar-link
+            :page="page"
+            :isActive="activePage === index"
+            @click.prevent="navLinkClick(index)"
+          ></navbar-link>
+        </li>
+      </ul>
+
+      <form class="d-flex">
+        <button class="btn btn-primary" @click.prevent="changeTheme()">
+          Toggle
+        </button>
+      </form>
+    </div>
+  </nav>
+</template>
+
+<script>
+import NavbarLink from "./NavbarLink.vue";
+
+export default {
+  components: {
+    NavbarLink,
+  },
+  props: ["pages", "activePage", "navLinkClick"],
+  data() {
+    return {
+      theme: "light",
+    };
+  },
+  methods: {
+    changeTheme() {
+      if (this.theme === "light") {
+        this.theme = "dark";
+      } else {
+        this.theme = "light";
+      }
+    },
+  },
+};
+</script>
